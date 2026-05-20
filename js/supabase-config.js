95e22589e9c68fffba47f4200d1b88b8358416e3
Craiaaan <dominguezlafuentecristian@fpllefia.com>
2026-05-20 12:51:45 +0200

    a

--- js/supabase-config.js
+++
@@ -1,8 +0,0 @@
-// js/supabase-config.js
-
-const supabaseUrl = "https://vfjpdcagugdazggdqiwl.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmanBkY2FndWdkYXpnZ2RxaXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNjMzMzAsImV4cCI6MjA5NDgzOTMzMH0._lkeY1sitvY1bBPmKjsz0RTFzx8msm530kHDk3YbKUU";
-
-// Inicializamos el cliente y lo guardamos en una variable global
-const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
